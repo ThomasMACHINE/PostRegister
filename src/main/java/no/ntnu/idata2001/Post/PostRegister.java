348a2af45a243b6f595b961b32a8feba93f27095
ThomasMACHINE <53544690+ThomasMACHINE@users.noreply.github.com>
2021-05-10 22:11:35 +0100

    Added postRegister class with accessor and mutator methods

---
+++ src/main/java/no/ntnu/idata2001/Post/PostRegister.java
@@ -0,0 +1,33 @@
+package no.ntnu.idata2001.Post;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PostRegister {
+    private String registerName; //Register name
+    private ArrayList<PostAddress> addresses; //ArrayList for holding all PostAddresses
+
+    public PostRegister(String registerName) {
+        this.registerName = registerName;
+        this.addresses = new ArrayList<>();
+    }
+
+    public String getRegisterName() {
+        return registerName;
+    }
+
+    public void setRegisterName(String registerName) {
+        //Check not null
+        Objects.requireNonNull(registerName, "Register Name cannot be null");
+        //Check not empty
+        if(registerName.equals("")){
+            throw new IllegalArgumentException("Register Name cannot be empty");
+        }
+        //Valid Register name
+        this.registerName = registerName;
+    }
+
+    public ArrayList<PostAddress> getAddresses() {
+        return addresses;
+    }
+}
