ea5ca8b192eba8f0c69ab7403881b03f47971ea3
ThomasMACHINE <53544690+ThomasMACHINE@users.noreply.github.com>
2021-05-11 21:44:52 +0100

    Implemented a new interface Displayable, with function getDisplayFields

---
+++ src/main/java/no/ntnu/idata2001/post/Displayable.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2001.post;
+
+import java.util.HashMap;
+
+/**
+ * Displayable interface is for all the classes of PostAddresses that will be displayed inside a TableView
+ * it's main purpose is to present data to the producers to allow the producer classes to format the object for any class
+ * in the same function
+ */
+public interface Displayable {
+
+    HashMap<String, String> getDisplayFields();
+}
