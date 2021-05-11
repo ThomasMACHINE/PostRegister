24113efa89aa3d3478f831550ff36dd4532d0667
ThomasMACHINE <53544690+ThomasMACHINE@users.noreply.github.com>
2021-05-11 04:16:16 +0100

    Added Main Menu bar to MainStage scene

--- src/main/java/no/ntnu/idata2001/gui/MainStage.java
+++ src/main/java/no/ntnu/idata2001/gui/MainStage.java
@@ -1,9 +1,11 @@
 package no.ntnu.idata2001.gui;
 
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import no.ntnu.idata2001.gui.factory.MenuProducer;
 import no.ntnu.idata2001.gui.factory.TableViewProducer;
 import no.ntnu.idata2001.post.PostAddress;
 
@@ -11,7 +13,8 @@ public class MainStage {
 
     public static Scene createMainStage(){
         TableView<PostAddress> postTable = TableViewProducer.create("PostAddress");
-        VBox vBox = new VBox(postTable);
+        MenuBar menuBar = MenuProducer.create("Main Menu", postTable);
+        VBox vBox = new VBox(menuBar, postTable);
         return new Scene(vBox);
     }
 }
