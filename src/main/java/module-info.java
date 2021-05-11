9f2c50203be62a3760d6d635f083bb3e8b776e7d
ThomasMACHINE <53544690+ThomasMACHINE@users.noreply.github.com>
2021-05-11 04:13:01 +0100

    Updated module info to open .gui and .post

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module PostRegister {
     requires javafx.graphics;
     requires javafx.controls;
 
-    opens no.ntnu.idata2001;
-
+    opens no.ntnu.idata2001.gui;
+    opens no.ntnu.idata2001.post;
     exports no.ntnu.idata2001;
 }
